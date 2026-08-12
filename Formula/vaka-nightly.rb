961011acd24518ae70c9cab730a8f6dfea4c1db5
Mariusz Woloszyn <emsi@users.noreply.github.com>
2026-08-12 20:00:25 +0200

    chore(formula): update vaka-nightly to 66bc469a8ad7

--- Formula/vaka-nightly.rb
+++ Formula/vaka-nightly.rb
@@ -1,38 +1,21 @@
 class VakaNightly < Formula
   desc "Declarative egress firewall for Docker containers (nightly)"
   homepage "https://github.com/infrasecture/vaka"
-  version "0.0.0-nightly.202605081655.6813c4a0ea0a"
+  version "0.0.0-nightly.202608121906.66bc469a8ad7"
   license "LGPL-2.1-only"
 
-  conflicts_with "vaka", because: "vaka and vaka-nightly install the same commands: vaka and vaka-init"
-
   on_arm do
-    url "https://github.com/infrasecture/vaka/releases/download/6813c4a0ea0a/vaka-brew-darwin-arm64.tar.gz"
-    sha256 "14a93bcb2fe7cfd2c9ef8af936bdd12bf27f08950fefecb1d51e5904cf22711e"
+    url "https://github.com/infrasecture/vaka/releases/download/66bc469a8ad7/vaka-brew-darwin-arm64.tar.gz"
+    sha256 "c0a9448adc1df701febb8904e997323a6d5ef90c9539bf17ba1338626aa15e81"
   end
 
   on_intel do
-    url "https://github.com/infrasecture/vaka/releases/download/6813c4a0ea0a/vaka-brew-darwin-amd64.tar.gz"
-    sha256 "dce650cebe754471a49ddfee30a43a8aa36dd121432a2906340541f622b73109"
+    url "https://github.com/infrasecture/vaka/releases/download/66bc469a8ad7/vaka-brew-darwin-amd64.tar.gz"
+    sha256 "7393ec3fe68b2bda6237c68d696a000a16915671a02778e7f09b32b10bf58175"
   end
 
   def install
     bin.install "vaka"
-    bin.install "vaka-init"
-  end
-
-  def caveats
-    <<~EOS
-      vaka and vaka-nightly install the same commands: vaka and vaka-init.
-      Only one channel should be linked at a time.
-
-      To switch from stable to nightly:
-        brew unlink vaka
-        brew install vaka-nightly
-
-      If nightly is already installed but unlinked:
-        brew link vaka-nightly
-    EOS
   end
 
   test do
